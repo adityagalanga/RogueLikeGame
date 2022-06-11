6c2bbe58cf0f03410dd5daccbf0361a908d5a6bb
Aditya Galang Adzana <adityagalang80@gmail.com>
2022-06-11 12:27:49 +0700

    Aiming character

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,9 +7,12 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     private Vector2 moveInput;
     public Rigidbody2D TheRB;
+    public Transform gunfire;
+    private Camera theCam;
+
     void Start()
     {
-        
+        theCam = Camera.main;
     }
 
     void Update()
@@ -20,5 +23,23 @@ public class PlayerController : MonoBehaviour
         //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
 
         TheRB.velocity = moveInput * moveSpeed;
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+        if (mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunfire.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunfire.localScale = Vector3.one;
+        }
+
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunfire.rotation = Quaternion.Euler(0,0,angle);
     }
 }
